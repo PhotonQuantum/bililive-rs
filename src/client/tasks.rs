486d1bb74c44b73644134c5362f26a8673864bd6
PhotonQuantum <self@lightquantum.me>
2021-06-21 21:42:53 +0800

    refactor(client::task): replace copy_within with drain

--- src/client/tasks.rs
+++ src/client/tasks.rs
@@ -55,10 +55,8 @@ impl Client {
                             IncompleteResult::Ok((remaining, raw)) => {
                                 println!("packet parsed, {} bytes remaining", remaining.len());
 
-                                let remaining_len = remaining.len();
-                                let split_pos = buf.len() - remaining_len;
-                                buf.copy_within(split_pos.., 0);
-                                buf.truncate(remaining_len);
+                                let consume_len = buf.len() - remaining.len();
+                                drop(buf.drain(..consume_len));
 
                                 let pack = Packet::from(raw);
                                 if pack.op() == Operation::HeartBeatResponse {
