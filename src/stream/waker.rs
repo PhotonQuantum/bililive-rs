31ffa3ed869ef77c070c092d8d4a7a1c6d9a0c4b
PhotonQuantum <self@lightquantum.me>
2021-07-07 05:05:54 +0800

    chore: reformat code

--- src/stream/waker.rs
+++ src/stream/waker.rs
@@ -3,11 +3,11 @@ use std::task::{Wake, Waker};
 
 use futures::task::AtomicWaker;
 
-// When reading the stream, a poll_ready is executed to ensure that all pending write op including
-// heartbeat is completed.
-// Therefore, we need to wake the task on which stream is polled in poll_ready (and poll_flush).
-// WakerProxy is a waker dispatcher. It will dispatch a wake op to both wakers (rx & tx), such that
-// both stream task and sink task can be waken and no starvation will occur.
+/// When reading the stream, a poll_ready is executed to ensure that all pending write op including
+/// heartbeat is completed.
+/// Therefore, we need to wake the task on which stream is polled in poll_ready (and poll_flush).
+/// WakerProxy is a waker dispatcher. It will dispatch a wake op to both wakers (rx & tx), such that
+/// both stream task and sink task can be waken and no starvation will occur.
 #[derive(Debug, Default)]
 pub struct WakerProxy {
     tx_waker: AtomicWaker,
