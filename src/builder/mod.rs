41a5c275f70904d9bc2f94359abcb3fee3e2116a
PhotonQuantum <self@lightquantum.me>
2021-09-25 21:01:51 +0800

    doc(builder): refine

--- src/builder/mod.rs
+++ src/builder/mod.rs
@@ -1,4 +1,4 @@
-//! `bililive` config builders.
+//! `bililive` config builder.
 
 use crate::builder::http::HTTPClient;
 use crate::builder::types::{ConfQueryInner, Resp, RoomQueryInner};
@@ -10,7 +10,7 @@ mod http;
 pub(crate) mod tests;
 mod types;
 
-/// `bililive` stream config builder
+/// `bililive` stream config builder.
 ///
 /// Stream config can be built via given live room parameters (room id and user id) & danmaku server configs (server token and list).
 ///
