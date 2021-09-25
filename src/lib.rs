8590d4e60f45a65ef4b83c36da9527f5b51311b0
PhotonQuantum <self@lightquantum.me>
2021-09-25 21:02:30 +0800

    doc: inline docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
+#[doc(inline)]
 pub use crate::builder::ConfigBuilder;
+#[doc(inline)]
 pub use crate::config::{RetryConfig, StreamConfig};
 pub use crate::connect::*;
+#[doc(inline)]
 pub use crate::errors::BililiveError;
+#[doc(inline)]
 pub use crate::packet::{Operation, Packet, Protocol};
+#[doc(inline)]
 pub use crate::stream::BililiveStream;
 
 #[macro_use]
